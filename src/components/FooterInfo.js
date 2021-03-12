9a609a2af1c59064ba3b61a58ed060d96a9f4f95
Molly Durst <mollymadegraphics@yahoo.com>
2021-03-12 08:23:22 -0500

    updated projects, added space to footer

--- src/components/FooterInfo.js
+++ src/components/FooterInfo.js
@@ -4,8 +4,14 @@ import React from "react";
 
 function FooterInfo() {
     return (
+    <div>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
     <div className="bg-dark text-white fixed-bottom footer mt-auto py-3">
     <span className="text-muted">Built for OSU Coding Bootcamp &bull; Right reserved under the MIT License</span>
+    </div>
     </div>
     );
 }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -8,24 +8,79 @@ import weather_icon from "../images/weather_icon.png";
 import fireworks_icon from "../images/fireworks_icon.png";
 import soccer_icon from "../images/soccer_icon.png";
 
-
 function Projects() {
   return (
-    <div>
-       <br/>
-      <h5 className="moreLeft">Recent coding projects I have worked on</h5>
-      <img className="projectImg" src={quiz_icon} alt="quiz_icon" />
-      <img className="projectImg" src={day_planner} alt="day_planner" />
-      <img className="projectImg" src={clock_icon} alt="clock_icon" />
+    <div className=".container-fluid">
+      <br/>
+    <h5 className="moreLeft">Recent coding projects I have worked on</h5>
+    <br/>
+      <div className="row">
+    <div className="col">
+    <img className="projectImg" src={quiz_icon} alt="quiz_icon" />
+    <br/>
+    <button className="btn btn-primary" data-toggle="button"><a href="https://mdurst365.github.io/Coding_Quiz/" target="_blank" rel="noreferrer">See it Live</a></button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button"><a href="https://github.com/mdurst365/Coding_Quiz" target="_blank" rel="noreferrer">View the Code</a></button>
+    </div>
+    <div className="col">
+    <img className="projectImg" src={day_planner} alt="day_planner" />
+    <br/>
+    <button className="btn btn-primary" data-toggle="button">See it Live</button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button">View the Code</button>
+    <br/>  
+    </div>
+    <div className="col">
+    <img className="projectImg" src={clock_icon} alt="clock_icon" />
+     <br/>
+    <button className="btn btn-primary" data-toggle="button">See it Live</button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button">View the Code</button>
+    <br/>  
+    </div>
+    <div className="col">
       <img className="projectImg" src={taco_beer_icon} alt="taco_beer_icon" />
-      
-      <img className="projectImg" src={hamburger_icon} alt="hamburger_icon" />
-      <img className="projectImg" src={weather_icon} alt="weather_icon" />
-      <img className="projectImg" src={fireworks_icon} alt="fireworks_icon" />
+      <br/>
+    <button className="btn btn-primary" data-toggle="button">See it Live</button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button">View the Code</button>
+    <br/> 
+      </div>
+    <div className="row">
+      <div className="col">
       <img className="projectImg" src={soccer_icon} alt="soccer_icon" />
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
+      <br/>
+    <button className="btn btn-primary" data-toggle="button">See it Live</button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button">View the Code</button>
+    <br/> 
+      </div>
+      <div className="col">
+      <img className="projectImg" src={hamburger_icon} alt="hamburger_icon" />
+      <br/>
+    <button className="btn btn-primary" data-toggle="button">See it Live</button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button">View the Code</button>
+    <br/> 
+      </div>
+      <div className="col">
+      <img className="projectImg" src={weather_icon} alt="weather_icon" />
+      <br/>
+    <button className="btn btn-primary" data-toggle="button">See it Live</button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button">View the Code</button>
+    <br/> 
+      </div>
+      <div className="col">
+      <img className="projectImg" src={fireworks_icon} alt="fireworks_icon" />
+      <br/>
+    <button className="btn btn-primary" data-toggle="button">See it Live</button>
+    <br/>
+    <button className="btn btn-info" data-toggle="button">View the Code</button>
+    <br/> 
+      </div>
+    </div>
+  </div>
     </div>
   );
 }

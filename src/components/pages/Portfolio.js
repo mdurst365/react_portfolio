8e718a1890f38a923df47f184bf2ef6d7ea01090
Molly Durst <mollymadegraphics@yahoo.com>
2021-03-15 18:23:26 -0400

    converted portfolio page to use cards and json data

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,32 +1,24 @@
-import React from "react";
-import bbq_flyer from "../images/bbq_flyer.jpg";
-import book_show from "../images/book_show.jpg";
-import boutique_flyer from "../images/boutique_flyer.jpg";
-import farmers_market from "../images/farmers_market.jpg";
-import LADIES_NIGHT from "../images/LADIES_NIGHT.jpg";
-import sale_flyer from "../images/sale_flyer.jpg";
-import sales_growth from "../images/sales_growth.jpg";
-import summer_festival from "../images/summer_festival.jpg";
+import React, { Component} from "react";
+import Cards from "./data/portfolioCards.json";
+import "../styles.css";
 
-function Portfolio() {
-  return (
-    <div>
-      <br/>
-      <h5 className="moreLeft">More work examples available by request</h5>
-      <img className="portfolioImg" src={bbq_flyer} alt="bbq_flyer" />
-      <img className="portfolioImg" src={book_show} alt="book_show" />
-      <img className="portfolioImg" src={boutique_flyer} alt="boutique_flyer" />
-      <img className="portfolioImg" src={farmers_market} alt="farmers_market" />
-      
-      <img className="portfolioImg" src={LADIES_NIGHT} alt="LADIES_NIGHT" />
-      <img className="portfolioImg" src={sale_flyer} alt="sale_flyer" />
-      <img className="portfolioImg" src={sales_growth} alt="sales_growth" />
-      <img className="portfolioImg" src={summer_festival} alt="summer_festival" />
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-    </div>
-  );
+class Portfolio extends Component {
+  render () {
+    return (
+      <div>
+        <br/>
+        <h2 className="moreLeft">Design Projects</h2>
+        {Cards.map ((cardDetail, index) => {
+          return <div key={index}>
+          <div className="Wrapper">
+          <h4>{cardDetail.cardname}</h4>
+          <img className="portfolioImg" src={cardDetail.image} alt="Portfolio Card" />
+          </div>
+          </div>
+        })}
+      </div>
+    )
+  }
 }
 
 export default Portfolio;
